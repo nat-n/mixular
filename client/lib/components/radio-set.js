7ac7b213cb8777718444c34042e4f5a16643452e
Nat Noordanus <n@natn.me>
2014-12-06 14:55:25 +0000

    added horizontal layout for radioset component

--- client/lib/components/radio-set.js
+++ client/lib/components/radio-set.js
@@ -14,21 +14,24 @@ angular.module('mixularApp')
     template: (
       '<fieldset>' +
         '<legend ng-show="mx.label" ng-bind="mx.label"></legend>' +
-        '<label class="radio" ng-repeat="o in mx.options">' +
-          '<input type="radio"' +
-                ' ng-model="mx.model[mx.key]"' +
-                ' name="{{mx.id}}"' +
-                ' id="{{mx.id}}-{{$index}}"' +
-                ' ng-value="o.value">' +
-          '{{o.label}}' +
-        '</label>' +
+        '<div>' +
+          '<label class="radio" ng-repeat="o in mx.options">' +
+            '<input type="radio"' +
+                  ' ng-model="mx.model[mx.key]"' +
+                  ' name="{{mx.id}}"' +
+                  ' id="{{mx.id}}-{{$index}}"' +
+                  ' ng-value="o.value">' +
+            '{{o.label}}' +
+          '</label>' +
+        '</div>' +
       '</fieldset>'
     ),
     targets: function(elem) {
       var fieldset = elem.children('fieldset')[0];
       return {
         main: fieldset,
-        legend: elem.find('legend')[0]
+        legend: elem.find('legend')[0],
+        labelsWrapper: elem.find('fieldset > div')[0]
       };
     }
   });
---
+++ client/lib/mixins/horizontalRadioSet.js
@@ -0,0 +1,46 @@
+
+angular.module('mixularApp')
+
+  .directive('horizontalRadioSet', function($templateCache, subTemplates, coreComponents) {
+    'use strict';
+
+    $templateCache.put('horizontal-radioset.html',
+      '<div class="form-group row horizontal-radioset">' +
+        '<div class="main-wrapper">' +
+          '<tp-before></tp-before>' +
+          '<tp-main></tp-main>' +
+          '<tp-after></tp-after>' +
+          '<tp-after-1></tp-after-1>' +
+        '</div>' +
+      '</div>'
+    );
+
+    subTemplates.register(
+      'horizontalRadioSet',
+      {priority: 150},
+      function(elem, attrs, targets) {
+        targets.$wrap('main', 'horizontal-radioset.html');
+        targets.legend.classList.add('col-md-2');
+        targets.labelsWrapper.classList.add('col-md-6');
+        targets.before = elem.find('tp-before')[0];
+        targets.after = elem.find('tp-after')[0];
+        targets.after1 = elem.find('tp-after-1')[0];
+        if (attrs.horizontalRadioSet === 'inline') {
+          target.labelsWrapper.find('input.radio')
+            .removeClass('radio')
+            .addClass('radio-inline');
+        }
+      }
+    );
+
+    return {
+      restrict: 'A',
+      priority: 150,
+      require: 'mxRadioSet',
+      link: function(scope, elem, attrs, ctrls) {
+        var ctrl;
+        if (!(ctrl = _.find(ctrls))) { return; }
+
+      }
+    };
+  });
